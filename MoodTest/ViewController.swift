23f4e932e34b5fcdd491edfecbb22160464fdfa9
Alonso <code@zhangwu.space>
2017-09-07 13:19:09 +0800

    First commit

---
+++ MoodTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MoodTest
+//
+//  Created by Alonso on 2017/9/7.
+//  Copyright © 2017年 Alonso. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
